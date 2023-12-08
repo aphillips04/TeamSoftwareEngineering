83f9db11625c69f85d5ba98e5af74e5fb5b38377
Alex Phillips <2004alexphillips@gmail.com>
2023-12-08 13:00:09 +0000

    Update Alien.cs

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -1,40 +1,97 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System;
+
 
 public class Alien : MonoBehaviour
 {
-    Color SkinColour;
-    float exampleTimer;
-    Material skinMat;
-    MeshRenderer meshRenderer;
-    // Start is called before the first frame update
+    // Alien configuration variables
+    public enum AlienType { Star, Insect, Mammal }
+    public AlienType alienType;
+    [Range(0, 10)]
+    public int happiness = 5, calmness = 5; // Higher is happier, higher is calmer
+
+    /// <summary>
+    /// Start is the initial setup function, called before the first frame update
+    /// </summary>
     void Start()
     {
-        exampleTimer = 0; 
-        
-        meshRenderer = GetComponent<MeshRenderer>();
-        skinMat = meshRenderer.material;
+        return;
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Update is the objects game loop function, called once per frame
+    /// </summary>
     void Update()
     {
-        skinMat.SetColor("_Color",SkinColour);
-        exampleTimer -= Time.deltaTime;
-        if (exampleTimer > 0)
+        if (alienType == AlienType.Star)
         {
-            SkinColour = new Color(255, 0, 0);
+            // Set the colour of the alien based on its happiness
+            Color skinColour = new Color(
+                .65f + (happiness *  .035f), // The change is calculated through: (end - start) / (steps - 1)
+                .00f + (happiness *  .100f), // There are 11 steps of from 0 to 11
+                .65f + (happiness * -.065f)  // The start value was RGB(166, 0, 166), end value was RGB(255, 255, 0) 
+            );
+            GetComponent<MeshRenderer>().material.color = skinColour;
 
+            // Set the spin speed of the alien based on its calmness
         }
-        else
+        else if (alienType == AlienType.Insect)
         {
-            SkinColour = new Color(255, 255, 255);
+            // Set how much teeth shown based on its happiness
+
+            // Set how much it moves around based on its calmness
+        }
+        else if (alienType == AlienType.Mammal)
+        {
+            // Set its purring or growling based on its happiness
+
+            // Set how much its tail is coiled based on its calmness
         }
     }
 
-    void React()
+    /// <summary>
+    /// How the alien responds to the players action
+    /// </summary>
+    void React(Tool tool)
     {
-        exampleTimer = 1.5f;
+        if (tool.toolType == Tools.Touch_Gently)
+        {
+            if (alienType == AlienType.Star)
+                happiness = Math.Min(10, happiness + 1);
+            else if (alienType == AlienType.Insect)
+                happiness = Math.Max(0, happiness - 1);
+            else if (alienType == AlienType.Mammal) // NOT DECIDED YET
+                return;
+        }
+        else if (tool.toolType == Tools.Touch_Roughly)
+        {
+            if (alienType == AlienType.Star)
+                happiness = Math.Min(10, happiness - 1);
+            else if (alienType == AlienType.Insect)
+                happiness = Math.Min(10, happiness + 1);
+            else if (alienType == AlienType.Mammal) // NOT DECIDED YET
+                return;
+        }
+        else if (tool.toolType == Tools.Feed_Treat)
+        {
+            if (alienType == AlienType.Star)
+                calmness = Math.Min(10, calmness + 1);
+            else if (alienType == AlienType.Insect)
+                calmness = Math.Max(0, calmness - 1);
+            else if (alienType == AlienType.Mammal) // NOT DECIDED YET
+                return;
+        }
+        else if (tool.toolType == Tools.Feed_LiveAnimal)
+        {
+            if (alienType == AlienType.Star)
+                calmness = Math.Min(10, calmness - 1);
+            else if (alienType == AlienType.Insect)
+                calmness = Math.Min(10, calmness + 1);
+            else if (alienType == AlienType.Mammal) // NOT DECIDED YET
+                return;
+        }
+        else if (tool.toolType == Tools.Oscilliscope)
+        {
+        }
     }
 }
