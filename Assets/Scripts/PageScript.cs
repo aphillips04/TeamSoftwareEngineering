4ef1577ecbceb5eda6d7a3cfd0eaa18bf725ced9
Jonny <jonnyrichards254@gmail.com>
2024-05-06 17:38:16 +0100

    Added left page switching by prefab (still needs instantiating on other script)

--- Assets/Scripts/PageScript.cs
+++ Assets/Scripts/PageScript.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class PageScript : MonoBehaviour
 {
+    public int numAliens;
+    public List<string> AllAlienNames;
+    public List<string> AllAlienDescriptions;
+    public GameObject AlienName;
+    public GameObject AlienDescription;
+    private TextMeshPro NameText;
+    private TextMeshPro DescText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        NameText = AlienName.GetComponent<TextMeshPro>();
+        DescText = AlienDescription.GetComponent<TextMeshPro>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void SelectPages(string name)
+    {
+        SelectLeftPage(name);
+        SelectRightPage(name);
+    }
+
+    private void SelectRightPage(string name)
+    {
+        //fill out combos
+        //yep
+        //storing combos could be tricky ill come back to this later
+    }
+
+    private void SelectLeftPage(string name)//make sure to call this when instantiating pages
+    {
+        if (!AllAlienNames.Contains(name))
+        {
+            Debug.Log("SelectLeftPage was given an invalid alien name!");
+            NameText.text = "ERROR";
+            DescText.text = "Invalid alien name given!";
+            return;
+        }
+        int idx = AllAlienNames.IndexOf(name);
+        NameText.text = AllAlienNames[idx];
+        DescText.text = AllAlienDescriptions[idx];
     }
 }
