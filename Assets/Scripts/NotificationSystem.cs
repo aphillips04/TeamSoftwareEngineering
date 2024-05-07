293edd8800f5b02fba6d1ed4bdaef3c2dd2ab2b3
OwlEggs <141333011+OwlEggs@users.noreply.github.com>
2024-05-07 16:30:39 +0100

    fix notification system

--- Assets/Scripts/NotificationSystem.cs
+++ Assets/Scripts/NotificationSystem.cs
@@ -2,15 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
 public class NotificationSystem : MonoBehaviour
 {
-    public GameObject notifications;
+    [SerializeField] private GameObject notifications;
+    private GameObject background;
     private GameObject textBox;
     private float timer;
     private float length;
     void Start()
     {
         NotifSys.system = this;
+
+        background = notifications.transform.GetChild(0).gameObject;
         textBox = notifications.transform.GetChild(1).gameObject;
         notifications.SetActive(false);
         textBox.GetComponent<TextMeshProUGUI>().color = Color.white;
@@ -27,10 +32,21 @@ public class NotificationSystem : MonoBehaviour
     }
     public void notify(string message, float period = 3)
     {
+        // Set variables
+        length = period;
+        timer = 0;
+
+        // Update text and show
         notifications.SetActive(true);
         textBox.GetComponent<TextMeshProUGUI>().text = message;
-        timer = 0;
-        length = period;
+
+        // Force update canvas to run content size fitter, calculating new height
+        Canvas.ForceUpdateCanvases();
+
+        // Update background to same dimensions as textbox
+        Vector2 size = textBox.GetComponent<RectTransform>().sizeDelta;
+        Debug.Log(size);
+        background.GetComponent<RectTransform>().sizeDelta = size;
     }
 }
 
