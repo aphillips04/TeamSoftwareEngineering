33407116813b929b3b17c16db4702f17ee723fe3
OwlEggs <141333011+OwlEggs@users.noreply.github.com>
2024-05-07 15:20:57 +0100

    show and hide notif system

--- Assets/Scripts/NotificationSystem.cs
+++ Assets/Scripts/NotificationSystem.cs
@@ -4,14 +4,33 @@ using TMPro;
 using UnityEngine;
 public class NotificationSystem : MonoBehaviour
 {
-    public GameObject textBox;
+    public GameObject notifications;
+    private GameObject textBox;
+    private float timer;
+    private float length;
     void Start()
     {
         NotifSys.system = this;
+        textBox = notifications.transform.GetChild(1).gameObject;
+        notifications.SetActive(false);
+        textBox.GetComponent<TextMeshProUGUI>().color = Color.white;
     }
-    public void notify(string message)
+    private void Update()
     {
+        if (!notifications.activeSelf) return;
+        timer += Time.deltaTime;
+        if (timer > length)
+        {
+            notifications.SetActive(false);
+        }
+        
+    }
+    public void notify(string message, float period = 3)
+    {
+        notifications.SetActive(true);
         textBox.GetComponent<TextMeshProUGUI>().text = message;
+        timer = 0;
+        length = period;
     }
 }
 
